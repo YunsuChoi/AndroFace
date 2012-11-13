91f3ed74a8b953d641bf12bca6556cf6f82fab60
Yunsu Choi <goodbye.tigris@gmail.com>
2012-11-14 04:17:45 +0900

    icon added

--- Main/gen/androface/nfc/android/apps/nfcreaderwriter/R.java
+++ Main/gen/androface/nfc/android/apps/nfcreaderwriter/R.java
@@ -10,29 +10,32 @@ package androface.nfc.android.apps.nfcreaderwriter;
 public final class R {
     public static final class attr {
     }
+    public static final class drawable {
+        public static final int ic_launcher=0x7f020000;
+    }
     public static final class id {
-        public static final int btnLed=0x7f060001;
-        public static final int keyCodeView=0x7f060002;
-        public static final int myUrl=0x7f060003;
-        public static final int myWriteUrlButton=0x7f060004;
-        public static final int txtMsg=0x7f060000;
+        public static final int btnLed=0x7f070001;
+        public static final int keyCodeView=0x7f070002;
+        public static final int myUrl=0x7f070003;
+        public static final int myWriteUrlButton=0x7f070004;
+        public static final int txtMsg=0x7f070000;
     }
     public static final class layout {
-        public static final int main=0x7f020000;
+        public static final int main=0x7f030000;
     }
     public static final class raw {
-        public static final int turret_autosearch_1=0x7f040000;
-        public static final int turret_disabled_2=0x7f040001;
-        public static final int turret_dispensing=0x7f040002;
-        public static final int wheatley_disappointing=0x7f040003;
-        public static final int wheatley_hello=0x7f040004;
+        public static final int turret_autosearch_1=0x7f050000;
+        public static final int turret_disabled_2=0x7f050001;
+        public static final int turret_dispensing=0x7f050002;
+        public static final int wheatley_disappointing=0x7f050003;
+        public static final int wheatley_hello=0x7f050004;
     }
     public static final class string {
-        public static final int app_name=0x7f050000;
-        public static final int hello=0x7f050001;
-        public static final int keyCode=0x7f050002;
+        public static final int app_name=0x7f060000;
+        public static final int hello=0x7f060001;
+        public static final int keyCode=0x7f060002;
     }
     public static final class xml {
-        public static final int accessory_filter=0x7f030000;
+        public static final int accessory_filter=0x7f040000;
     }
 }
